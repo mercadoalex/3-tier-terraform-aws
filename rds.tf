3a40b33a6b13a9d9cb48e94780ba2b68cad0287d
Alex <mercadoalex@gmail.com>
2024-12-04 14:29:02 -0600

    Update rds.tf
    v2

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 ##### database subnet group ####
 
-resource "aws_db_subnet_group" "database-subnet-group" {
+resource "aws_db_subnet_group" "database-subnet-group2" {
   name       = "database subnets"
   subnet_ids = ["${aws_subnet.private-db-subnet-1.id}", "${aws_subnet.private-db-subnet-2.id}"]
 
