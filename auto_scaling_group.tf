c37ad08a490ec2412b4a5fba52fd530d899c2dcb
mercadoalex <mercadoalex@gmail.com>
2024-12-09 13:14:10 -0600

    ajustamos el auto scaling group

--- auto_scaling_group.tf
+++ auto_scaling_group.tf
@@ -1,7 +1,7 @@
 #######ASG for Presentation Tier ######
 resource "aws_launch_template" "auto-scaling-group-public" {
   #count         = var.instance_count_autoscaling
-  name_prefix   = "auto-scaling-group"
+  name_prefix   = "auto-scaling-group-public"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name      = var.key_pair_name
@@ -48,20 +48,32 @@ resource "aws_autoscaling_group" "auto-scaling-group-public" {
 }
 
 ######## ASG for Application Tier ########
-resource "aws_launch_configuration" "auto-scaling-group-private" {
+resource "aws_launch_template" "auto-scaling-group-private" {
+  #count         = var.instance_count_autoscaling
   name_prefix   = "auto-scaling-group-private"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name      = var.key_pair_name
-
-  security_groups = [aws_security_group.ssh-aws-security-group.id]
-
+  network_interfaces {
+    subnet_id                   = aws_subnet.private-app-subnet-1.id
+    security_groups             = [aws_security_group.ssh-aws-security-group.id]
+    associate_public_ip_address = true
+  }
   user_data = base64encode(templatefile("${path.root}/web-setup-test.sh", {
+    #file_content = "version 1.0 - #${count.index}"
     file_content = "version 1.0 - auto-scaling"
   }))
-
-  lifecycle {
-    create_before_destroy = true
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.instance_name_prefix}-asg"
+    }
+  }
+  tag_specifications {
+    resource_type = "volume"
+    tags = {
+      Name = "${var.instance_name_prefix}-asg"
+    }
   }
 }
 
@@ -69,14 +81,17 @@ resource "aws_autoscaling_group" "auto-scaling-group-private" {
   desired_capacity = 2
   max_size         = 5
   min_size         = 1
-
-  launch_configuration = aws_launch_configuration.auto-scaling-group-private.id
-
+  #id = aws_launch_template.auto-scaling-group-private.id
   vpc_zone_identifier = [aws_subnet.private-app-subnet-1.id, aws_subnet.private-app-subnet-2.id]
 
   tag {
     key                 = "Name"
     value               = "${var.instance_name_prefix}-asg-private"
     propagate_at_launch = true
+  }
+  launch_template {
+    id = aws_launch_template.auto-scaling-group-private.id
+    #id      = aws_launch_template.auto-scaling-group-public.id[count.index]
+    version = "$Latest"
   }
 }
